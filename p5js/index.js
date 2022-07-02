d2ecdc94712bcd9fb084dea52acc2b29f531e440
miaoqvmiao <99528572+miaoqvmiao@users.noreply.github.com>
2022-07-03 07:27:50 +0800

    Update index.js

--- p5js/index.js
+++ p5js/index.js
@@ -4,6 +4,8 @@ function setup(){
   strokeWeight(2)
   var w = width-1
   var h = height-1
+}
+function draw(){
   line(1,1,1,w)
   line(1,w,w,h)
   line(w,h,w,1)
@@ -11,7 +13,4 @@ function setup(){
   for (let i;i = 13;i++){
      line((w/13)*i,1,(w/13)*i,h)
   }
-}
-function draw(){
-  
 }
