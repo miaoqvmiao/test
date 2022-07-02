270f537c46c264329e6ff2d9365aac32f4651146
miaoqvmiao <99528572+miaoqvmiao@users.noreply.github.com>
2022-07-02 16:24:48 +0800

    Update and rename main.js to index.js

--- p5js/index.js
+++ p5js/index.js
@@ -1,12 +1,17 @@
 function setup(){
   createCanvas(720,720)
-}
-function draw(){
-  background(255)
+  background(255,193,37)
+  strokeWeight(2)
   var w = width-1
   var h = height-1
   line(1,1,1,w)
   line(1,w,w,h)
   line(w,h,w,1)
   line(w,1,1,1)
+  for (let i;i = 13;i++){
+     line((w/13)*i,1,(w/13)*i,h)
+  }
+}
+function draw(){
+  
 }
