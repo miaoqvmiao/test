f8f7db413a9b36cf624aa3458642e94e85cf2df5
miaoqvmiao <99528572+miaoqvmiao@users.noreply.github.com>
2022-07-02 14:46:07 +0800

    Update main.js

--- p5js/main.js
+++ p5js/main.js
@@ -1,12 +1,11 @@
 function setup(){
-  createCanvas(700,700)
+  createCanvas(1000,1000)
 }
 function draw(){
-  background(0)
-  fill(255)
+  background(255)
+  fill(0)
   var w = width-1
   var h = height-1
-  fill(255)
   line(1,1,1,w)
   line(1,w,w,h)
   line(w,h,w,1)
