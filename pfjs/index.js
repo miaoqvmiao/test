6ee3428a5dc509f9456eb29485d20b79ca08146e
miaoqvmiao <99528572+miaoqvmiao@users.noreply.github.com>
2022-07-03 12:32:58 +0800

    Update index.js

--- pfjs/index.js
+++ pfjs/index.js
@@ -5,11 +5,11 @@ function setup(){
   var w = width-1
   var h = height-1
   let i = 1
-  for (;i < 13;i++){
+  for (;i < 12;i++){
      line((w/13)*i,2,(w/13)*i,h)
   }
   i = 1
-  for (;i < 13;i++){
+  for (;i < 12;i++){
      line(2,(h/13)*i,w,(h/13)*i)
   }
 }
