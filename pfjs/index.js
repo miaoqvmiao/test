318a8dd5880ed62dcaebc1d01d3db5f35b070c5c
miaoqvmiao <99528572+miaoqvmiao@users.noreply.github.com>
2022-07-03 11:29:28 +0800

    Update index.js

--- pfjs/index.js
+++ pfjs/index.js
@@ -4,11 +4,13 @@ function setup(){
   strokeWeight(3)
   var w = width-1
   var h = height-1
+  let i = 1
   rect(0,0,650,650)
-  for (let i;i < 14;i++){
+  for (;i < 14;i++){
      line((w/13)*i,1,(w/13)*i,h)
   }
-  for (let i;i < 14;i++){
+  i = 1
+  for (;i < 14;i++){
      line((1,(h/13)*i,w,(h/13)*i)
   }
 }
