3b0d48b6a18c870641c98679f489d86b89f9abeb
miaoqvmiao <99528572+miaoqvmiao@users.noreply.github.com>
2022-07-03 11:28:34 +0800

    Update index.js

--- pfjs/index.js
+++ pfjs/index.js
@@ -2,12 +2,16 @@ function setup(){
   createCanvas(700,700)
   background(255,193,37)
   strokeWeight(3)
-}
-function draw(){
   var w = width-1
   var h = height-1
   rect(0,0,650,650)
   for (let i;i < 14;i++){
      line((w/13)*i,1,(w/13)*i,h)
   }
+  for (let i;i < 14;i++){
+     line((1,(h/13)*i,w,(h/13)*i)
+  }
+}
+function draw(){
+  
 }
